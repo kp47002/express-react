282e54fed8626fc13bf7f69950851df6f8722948
unknown <kp47003@gmail.com>
2019-04-06 14:59:25 +0200

    add input for product details  on sell form

--- react/src/components/Sell/Sell.js
+++ react/src/components/Sell/Sell.js
@@ -12,12 +12,38 @@ class Sell extends Component {
     this.state = {
       products: [],
       productId: this.props.match.params.productId | "",
-      activeUser: cookies.get("loginCookie") || ""
+      activeUser: cookies.get("loginCookie") || "",
+      formSell: false,
+      name: "",
+      description: "",
+      price: 0.0
     };
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeDescription = this.handleChangeDescription.bind(this);
+    this.handleChangePrice = this.handleChangePrice.bind(this);
+  }
+
+  handleChangeName(event) {
+    console.log("handleChange");
+    console.log(event);
+    this.setState({ name: event.target.value });
+  }
+  handleChangeDescription(event) {
+    console.log("handleChange");
+    console.log(event);
+    this.setState({ description: event.target.value });
+  }
+  handleChangePrice(event) {
+    console.log("handleChange");
+    console.log(event);
+    this.setState({ price: event.target.value });
   }
   componentDidMount() {
     this.getProducts();
   }
+  formSell = () => {
+    this.setState({ formSell: true });
+  };
 
   addProduct = () => {
     console.log("this.post");
@@ -30,14 +56,14 @@ class Sell extends Component {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        name: "req.body.name",
-        description: "req.body.description",
-        price: 2,
+        name: this.state.name,
+        description: this.state.description,
+        price: this.state.price,
         username: this.state.activeUser
       })
     }).then(response => {
       console.log(response);
-
+      this.setState({ formSell: false });
       this.getProducts();
       //this.forceUpdate();
     });
@@ -68,8 +94,32 @@ class Sell extends Component {
     if (this.state.activeUser == "") {
       sell = <p>Login to sell</p>;
     } else {
-      sell = <button onClick={() => this.addProduct()}>Add</button>;
+      if (this.state.formSell == false) {
+        sell = <button onClick={() => this.formSell()}>Add</button>;
+      } else {
+        sell = (
+          <div>
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChangeName}
+            />
+            <input
+              type="text"
+              value={this.state.description}
+              onChange={this.handleChangeDescription}
+            />
+            <input
+              type="text"
+              value={this.state.price}
+              onChange={this.handleChangePrice}
+            />
+            <button onClick={() => this.addProduct()}>Add</button>;
+          </div>
+        );
+      }
     }
+    console.log("sell: " + sell);
     return (
       <div>
         Buy
